e0c7bced41e85b19d1153c335c3a2fe2ee5936a2
Harshitha <152185823+KachapuramHarshitha123@users.noreply.github.com>
2026-05-17 10:44:54 +0530

    Fixed CORS for localhost and Vercel

--- Backend/API/CompilerAPI.js
+++ Backend/API/CompilerAPI.js
@@ -1,9 +1,9 @@
 import exp from "express";
 import axios from "axios";
 
-export const compilerApp = exp.Router();
+const compilerApp = exp.Router();
 
-compilerApp.post("/run", async(req,res)=>{
+compilerApp.post("/run", async (req,res)=>{
 
 try{
 
@@ -13,41 +13,75 @@ language_id,
 stdin
 }=req.body;
 
-const response = await axios.post(
-
-"https://judge0-ce.p.rapidapi.com/submissions?base64_encoded=false&wait=true",
-
-{
-source_code,
-language_id,
-stdin
-},
-
-{
-headers:{
-"Content-Type":"application/json"
-}
-}
-
+console.log(
+"Received:",
+req.body
 );
 
-res.send(response.data);
+const response =
+await axios({
+
+method:"POST",
+
+url:"https://emkc.org/api/v2/piston/execute",
+
+data:{
+
+language:
+language_id===71 ? "python" :
+language_id===54 ? "cpp" :
+language_id===50 ? "c" :
+language_id===62 ? "java" :
+language_id===63 ? "javascript" :
+"python",
+
+version:"*",
+
+
+files:[
+
+{
+content:source_code
+}
+
+],
+
+stdin:stdin
+
+}
+
+});
+
+res.json({
+
+stdout:
+response.data.run.output
+
+});
 
 }
 
 catch(err){
 
 console.log(
-"Compiler error:",
-err.response?.data || err.message
+"Compiler Error:",
+err.response?.data ||
+err.message
 );
 
 res.status(500).json({
 
-message:"Compilation Failed"
+message:
+"Compilation Failed",
+
+error:
+err.response?.data ||
+err.message
 
 });
 
 }
 
 });
+
+export { compilerApp };
